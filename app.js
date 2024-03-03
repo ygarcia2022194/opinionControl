9af9ee8950540c28fe7480129e494af11b20b5fa
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-03-03 15:02:43 -0600

    Se hizo la conexion a la base de datos

---
+++ app.js
@@ -0,0 +1,6 @@
+import { config } from "dotenv";
+config();
+import Server from "./configs/server.js";
+
+const server = new Server();
+server.listen();
---
+++ configs/server.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js'
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+
+        this.middlewares();
+        this.conectarDB();
+        this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes(){
+        
+        
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+
+export default Server;
